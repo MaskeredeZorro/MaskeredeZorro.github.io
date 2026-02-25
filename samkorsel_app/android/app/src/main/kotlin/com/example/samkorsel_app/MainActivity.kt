ab131f39a37e5f46af0871bcc0970d42b7e184ea
Alexander Strarup Kristensen <alexander.strarup.kristensen1@sallinggroup.com>
2026-02-25 09:04:21 +0100

    Removed samkorsel_app (moved to Programmer repo)

--- samkorsel_app/android/app/src/main/kotlin/com/example/samkorsel_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.samkorsel_app // <--- Tjek at dette matcher din mappe-struktur
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
